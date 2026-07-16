37582a6c2b2e8f7f778243907ab5eee1b0588c66
NilsPils06 <135322818+NilsPils06@users.noreply.github.com>
2026-07-16 19:55:13 +0200

    Update main.cpp to work with the new Clock class

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,50 @@
-#include <ftxui/component/event.hpp>
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/color.hpp>
-#include <ftxui/screen/screen.hpp>
+#include "modules/clock.h"
+#include <chrono>
+#include <string>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
-#include <ftxui/component/loop.hpp>
-#include <chrono>
-#include <ctime>
-#include <iomanip>
-#include <sstream>
-#include <string>
+#include <ftxui/dom/elements.hpp>
+#include <thread>
 
 using namespace ftxui;
 
-std::string get_current_time() {
-    auto now = std::chrono::system_clock::now();
-    std::time_t now_t = std::chrono::system_clock::to_time_t(now);
-    std::tm* now_tm = std::localtime(&now_t);
-    std::stringstream sstream;
-    sstream << std::put_time(now_tm, "%H:%M:%S");
-
-    return sstream.str();
-}
-
 int main() {
+    dsh::Clock clock{};
+
     auto screen = ScreenInteractive::Fullscreen();
 
-    auto renderer = Renderer([&] {
-        std::string current_time = get_current_time();
-        return vbox({
-            text("Druk op 'u' om de tijd bij te werken") | border,
-            text("Huidige tijd: " + current_time) | color(Color::Green) | border,
-            text("Druk op 'q' om te sluiten") | dim
-        });
+    std::thread refresh([&screen]() {
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            screen.PostEvent(Event::Custom);
+        }
     });
 
-    screen.Loop(renderer);
+    refresh.detach();
+
+    auto renderer = Renderer([&] {
+        return vbox({
+            text(" D-SH Dashboard ") | bold | color(Color::Blue) | border,
+            separator(),
+            hbox({
+                text(" Current time: ") | color(Color::White),
+                text(" " + clock.get_time() + " ") | color(Color::Green) | bold,
+            }),
+            separator(),
+            text(" [q] Close app") | dim,
+        }) | border;
+    });
+
+    auto component = CatchEvent(renderer, [&](Event event) {
+        if (event == Event::Character('q')) {
+            screen.Exit();
+            return true;
+        }
+
+        return false;
+    });
+
+    screen.Loop(component);
 
     return 0;
 }
